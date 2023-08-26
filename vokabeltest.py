4ecf167d452c1ffdfae3a938a061860d6c5e7feb
luisluet <luis@luetkehellweg.de>
2023-08-26 18:56:24 +0200

    Add files via upload

---
+++ vokabeltest.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Erstelle einen Vokabeltest. Nutze '| pbcopy', um direkt in die Zwischenablage zu kopieren.")
+    parser.add_argument('size', type=int, help='Anzahl abgefragter Vokabeln', metavar='n', default=16)
+    parser.add_argument('lektionen', type=int, nargs='*', default=range(1,27), help='Lektionen, aus denen die abgefragten Woerter kommen', choices=range(1,27))
+    args = parser.parse_args()
+
+    df = pd.read_csv("Kreuzer.csv")
+    set = df[df.lektion.isin(args.lektionen)]
+    n = args.size
+    df_sub = set.sample(n)
+    #print("\\section{Vokabeltest mit " + str(args.size) + " Vokabeln aus Lektion " + str(args.lektionen) + "}")
+    print("\\section{Vokabeltest}")
+    print("\\begin{multicols}{2}")
+    print("{\\setstretch{1.7} \\raggedright")
+    for i in range(n):
+        if(pd.isna(df_sub.iloc[i]['bemerkungen'])):
+            print("\\LargeHebrew{" + df_sub.iloc[i]['hebraeisch'] + "}\\\\")
+        else:
+            print(df_sub.iloc[i]['bemerkungen'] + " \\LargeHebrew{" + df_sub.iloc[i]['hebraeisch'] + "}\\\\")
+    print("}")
+    print("\\end{multicols}")
+
+
+if __name__ == "__main__":
+    main()
