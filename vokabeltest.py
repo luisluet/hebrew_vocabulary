48723195afc403704a13cd7f9050a882be1ab8e5
luisluet <luis@luetkehellweg.de>
2025-04-15 07:48:16 +0200

    Update vokabeltest.py
    Option für Lehramt hinzufügen.

--- vokabeltest.py
+++ vokabeltest.py
@@ -4,11 +4,17 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Erstelle einen Vokabeltest. Nutze '| pbcopy', um direkt in die Zwischenablage zu kopieren.")
     parser.add_argument('size', type=int, help='Anzahl abgefragter Vokabeln', metavar='n', default=16)
+    parser.add_argument('--la', help="Falls der Test aus der gekürzten Liste erstellt werden soll. Dann Vokabelrange nur (1,26).", action="store_true")
     parser.add_argument('lektionen', type=int, nargs='*', default=range(1,27), help='Lektionen, aus denen die abgefragten Woerter kommen', choices=range(1,27))
     parser.add_argument("--solution", action="store_true", help="displays solution on new page")
     args = parser.parse_args()
 
-    df = pd.read_csv("Kreuzer.csv")
+    #wählt richtige Liste
+    if args.la:
+        df = pd.read_csv("Kreuzer_LA_BA.csv")
+    else:
+        df = pd.read_csv("Kreuzer.csv")
+
     set = df[df.lektion.isin(args.lektionen)]
     n = args.size
     df_sub = set.sample(n)
